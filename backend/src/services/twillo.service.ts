396fb04f2b699c60752e4ec04f8b1a7bd9316ba2
AbdulazizRus <coderdim@gmail.com>
2025-08-21 15:40:26 +0500

    otp phone number

---
+++ backend/src/services/twillo.service.ts
@@ -0,0 +1,29 @@
+import twillo from "twilio";
+
+const accountSid = process.env.TWILLO_ACCOUNT_CID as string;
+const authToken = process.env.TWILLO_AUTH_TOKEN as string;
+const serviceSid = process.env.TWILLO_SERVICE_CID as string;
+
+const client = twillo(accountSid, authToken);
+
+const sendOtpToPhoneNumber = async (phoneNumber: string) => {
+  try {
+    console.log("Sending OTP to phone number", phoneNumber);
+    if (!phoneNumber) {
+      throw new Error("Phone number is required");
+    }
+
+    const res = await client.verify.v2
+      .services(serviceSid)
+      .verifications.create({
+        to: phoneNumber,
+        channel: "sms",
+      });
+
+    console.log("this is my otp response", res);
+    return res;
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to send OTP to phone number");
+  }
+};
